d12c97b00607755ffd74c7d07f7f6d2f1e26f8fa
Aiden Scandella <git@sca.ndella.com>
2017-07-03 00:39:04 -0700

    make it pretty

--- template.go
+++ template.go
@@ -5,13 +5,23 @@ import "html/template"
 var sleepTemplate = template.Must(template.New("main").Parse(`
 <head>
 <title>the sleep of ai</title>
-<link rel="stylesheet" type="text/css" href="https://necolas.github.io/normalize.css/latest/normalize.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 </head>
 <body>
-  <h2>Date<h2>
-  <p>{{ .Date }} </p>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container">
+		</div>
+	</nav>
 
-  <h2>Time</h2>
-  <div>{{ .MostRecent }}</div>
+	<div class="container">
+		<h1>How much sleep did Aiden get?</h1>
+		<p class="lead">{{ .MostRecent }}</p>
+
+		<h5>Date</h5>
+		<p>{{ .Date }}</p>
+
+	</div>
 </body>
 `))
