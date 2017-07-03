0f934793f56c69eada38a06b7498f2814d0c4f6e
Aiden Scandella <git@sca.ndella.com>
2017-07-03 11:14:38 -0700

    Add meta tags

--- template.go
+++ template.go
@@ -8,6 +8,10 @@ var sleepTemplate = template.Must(template.New("main").Parse(`
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<meta property="og:title" content="aiden's sleep" />
+<meta property="og:description" content="{{ .FriendlyDuration }}" />
+
 </head>
 <body>
 
