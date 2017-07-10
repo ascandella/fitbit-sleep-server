de41fcc761374715cd9b0a155a3df61d927e79a6
Aiden Scandella <git@sca.ndella.com>
2017-07-09 17:28:23 -0700

    Refactor and test more

--- http_test.go
+++ http_test.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"os"
 	"testing"
+
+	"go.uber.org/zap"
 
 	"golang.org/x/oauth2"
 
+	"github.com/sectioneight/fitbit-sleep-server/internal/testutils"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewHandler_OK(t *testing.T) {
-	h := newHandler(oauth2.Config{}, appConfig{})
+	h := emptyHandler()
 	assert.NotEmpty(t, h.state)
 }
 
 func TestRegisterServMux(t *testing.T) {
-	registerServeMux(newHandler(oauth2.Config{}, appConfig{}))
+	registerServeMux(emptyHandler())
+}
+
+func TestRegisterToken_OK(t *testing.T) {
+	h := emptyHandler()
+
+	tkn := &oauth2.Token{
+		AccessToken: "foo",
+	}
+
+	testutils.WithTempDir(t, func(dir string) {
+		testutils.WithFile(t, dir, func(file *os.File) {
+			fname := file.Name()
+			h.appConfig.tokenFile = &fname
+			h.registerToken(tkn)
+		})
+	})
+}
+
+func emptyHandler() *myHandler {
+	l, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	return newHandler(oauth2.Config{}, appConfig{
+		log: l,
+	})
 }
--- internal/testutils/temp_file.go
+++ internal/testutils/temp_file.go
@@ -9,15 +9,27 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func WithFile(t testing.TB, dir string, fn func(*os.File)) {
+	f, err := ioutil.TempFile(dir, "withfilefitbit")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	fn(f)
+}
+
 func WithJSONFile(t testing.TB, in interface{}, f func(path string)) {
+	WithTempDir(t, func(dir string) {
+		WithFile(t, dir, func(file *os.File) {
+			e := json.NewEncoder(file)
+			require.NoError(t, e.Encode(in), "unable to encode JSON from struct")
+			f(file.Name())
+		})
+	})
+}
+
+func WithTempDir(t testing.TB, fn func(path string)) {
 	dir, err := ioutil.TempDir("", "fitbitsleeptest")
 	require.NoError(t, err, "Unable to create temp directory)")
-	file, err := ioutil.TempFile(dir, "jsonfile")
-	require.NoError(t, err, "Couldn't create tempfile")
-	e := json.NewEncoder(file)
-	require.NoError(t, e.Encode(in), "unable to encode JSON from struct")
-	defer os.Remove(file.Name())
 	defer os.RemoveAll(dir)
 
-	f(file.Name())
+	fn(dir)
 }
